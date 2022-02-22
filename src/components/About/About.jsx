e9d1c7a0e3ec7c815ecc037cc6f553e4e055354c
saufiroja <saufiroja08@gmail.com>
2022-02-22 10:55:23 +0800

    added about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,77 @@
+import { GitHub, Instagram, LinkedIn } from '@mui/icons-material';
+import { Box, Button, Container, Grid, Link, Typography } from '@mui/material';
 import React from 'react';
+
+import Logo from '../../assets/logo.svg';
 
 const About = () => {
   return (
     <>
-      <h1>Halaman About</h1>
+      <Container maxWidth='lg'>
+        <Box px={{ xs: 5, sm: 5 }} py={{ xs: 5, sm: 5 }}>
+          <Typography variant='h5' align='center'>
+            About Me
+          </Typography>
+        </Box>
+        <Grid container spacing={3} borderBottom={1} paddingBottom={10}>
+          <Grid item xs={12} sm={4} md={4}>
+            <img src={Logo} alt='' />
+          </Grid>
+          <Grid item xs={12} sm={8} md={8}>
+            <Box>
+              <Typography variant='h6'>
+                Hello, My name is Muhammad Saufi Roja, you can call me oja. I'm
+                a backend developer. I am currently 20 years old and I live in
+                Banjarmasin, South Kalimantan. I started learning about web
+                programming in 2021 with my first language being javascript, but
+                now I'm more interested and explore a lot about backend
+                development, especially on node.js
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+        <Typography variant='body2' pt={1}>
+          You can reach me via Email at{' '}
+          <Link href='mailto:muhammadsaufiroja@gmail.com'>
+            muhammadsaufiroja@gmail.com
+          </Link>{' '}
+          for more information, or you can find me on my social media:
+        </Typography>
+        <Box display='flex' px={{ xs: 10, sm: 1 }} py={{ xs: 5, sm: 2 }}>
+          <Box pr={3}>
+            <Link href='https://www.instagram.com/saufiroja_' underline='none'>
+              <Button variant='contained' color='error'>
+                <Instagram fontSize='small' /> Instagram
+              </Button>
+            </Link>
+          </Box>
+          <Box px={3}>
+            <Link href='https://github.com/saufiroja' underline='none'>
+              <Button
+                variant='contained'
+                sx={{
+                  backgroundColor: 'gray',
+                  ':hover': {
+                    backgroundColor: 'gray',
+                  },
+                }}
+              >
+                <GitHub fontSize='small' /> Github
+              </Button>
+            </Link>
+          </Box>
+          <Box px={3}>
+            <Link
+              href='https://www.linkedin.com/in/muhammad-saufi-roja-11427a1b8/'
+              underline='none'
+            >
+              <Button variant='contained'>
+                <LinkedIn fontSize='small' /> LinkedIn
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+      </Container>
     </>
   );
 };
