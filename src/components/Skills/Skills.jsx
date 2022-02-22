d165fc065de8b65572d45685202ed84e739ad4ee
saufiroja <saufiroja08@gmail.com>
2022-02-22 08:22:47 +0800

    added skils routes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,85 @@
+import { Box, Container, Grid, Typography } from '@mui/material';
 import React from 'react';
+
+const languages = ['Javascript', 'Typescript'];
+const frontend = [
+  'HTML & CSS',
+  'Bootstrap',
+  'Material UI',
+  'React JS',
+  'Next JS',
+];
+const backend = ['Node.js', 'Mongo DB', 'PostgreSQL', 'Nest Js'];
+const tools = ['Git', 'Postman', 'Trello'];
 
 const Skills = () => {
   return (
     <>
-      <h1>Halaman Skills</h1>
+      <Box px={{ xs: 5, sm: 5 }} py={{ xs: 5, sm: 3 }}>
+        <Container maxWidth='md'>
+          <Typography variant='h5' align='center'>
+            Skill
+          </Typography>
+          <Grid container spacing={3} mt={1}>
+            <Grid item xs={12} md={3}>
+              <Box borderBottom={1}>
+                <Typography variant='h5' align='center'>
+                  Languages
+                </Typography>
+              </Box>
+              {languages.map((language) => {
+                return (
+                  <Box mt={1} key={language}>
+                    <Typography>{language}</Typography>
+                  </Box>
+                );
+              })}
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Box borderBottom={1}>
+                <Typography variant='h5' align='center'>
+                  Frontend
+                </Typography>
+              </Box>
+              {frontend.map((frontend) => {
+                return (
+                  <Box mt={1} key={frontend}>
+                    <Typography>{frontend}</Typography>
+                  </Box>
+                );
+              })}
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Box borderBottom={1}>
+                <Typography variant='h5' align='center'>
+                  Backend
+                </Typography>
+              </Box>
+              {backend.map((backend) => {
+                return (
+                  <Box mt={1} key={backend}>
+                    <Typography>{backend}</Typography>
+                  </Box>
+                );
+              })}
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Box borderBottom={1}>
+                <Typography variant='h5' align='center'>
+                  Tools
+                </Typography>
+              </Box>
+              {tools.map((tool) => {
+                return (
+                  <Box mt={1} key={tool}>
+                    <Typography>{tool}</Typography>
+                  </Box>
+                );
+              })}
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
     </>
   );
 };
