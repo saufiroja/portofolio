84a303633f51dcc6b61dc5c6016dce28a7917c51
saufiroja <saufiroja08@gmail.com>
2022-02-23 22:36:55 +0800

    add education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -4,11 +4,29 @@ import React from 'react';
 const Education = () => {
   return (
     <>
-      <Container>
+      <Container maxWidth='lg'>
         <Box px={{ xs: 5, sm: 5 }} py={{ xs: 5, sm: 3 }}>
           <Typography variant='h5' align='center'>
             Education
           </Typography>
+        </Box>
+      </Container>
+      <Container maxWidth='lg'>
+        <Box pb={1}>
+          <Typography variant='h6'>Full-Stack Web Development</Typography>
+        </Box>
+        <Box borderBottom={1} pb={1}>
+          <Typography variant='body1'>Binar Academy</Typography>
+          <Typography variant='body1'>
+            17 August 2021 - 16 Maret 2022
+          </Typography>
+        </Box>
+        <Box pb={1} pt={1}>
+          <Typography variant='h6'>Back End Development and API</Typography>
+        </Box>
+        <Box borderBottom={1} pb={1}>
+          <Typography variant='body1'>FreeCodeCamp</Typography>
+          <Typography variant='body1'>01 December 2021 - NOW</Typography>
         </Box>
       </Container>
     </>
