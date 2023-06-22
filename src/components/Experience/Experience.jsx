091f00a46b57b2912cc34673bbe5e31d08b79b37
Muhammad Saufi Roja <80515361+saufiroja@users.noreply.github.com>
2023-06-22 19:50:25 +0700

    update

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -39,25 +39,6 @@ const Experience = () => {
             </>
           );
         })}
-        <Box pb={1}>
-          <Typography variant='h6'>Back End Developer</Typography>
-        </Box>
-        <Box borderBottom={1} pb={1}>
-          <Typography variant='body1'>PT ICE Messanger Indonesia</Typography>
-          <Typography variant='body1' pb={1}>
-            August 2022 - Present
-          </Typography>
-          <Typography variant='body1' sx={{ color: 'gray' }}>
-            - The focus will be on back/end Go development
-            <br />
-            - Designing and developing advanced backend platform
-            <br />
-            - Maintain code quality above the bar Fix and improve application
-            performance
-            <br />- Cooperating with the team in the process of building the
-            application
-          </Typography>
-        </Box>
       </Container>
     </>
   );
